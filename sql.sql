765d71392b4d5b65e7552c575f6d9509db51ba00
Princysundhar <princyz873@gmail.com>
2023-08-27 17:02:02 +0530

    commit

---
+++ sql.sql
@@ -0,0 +1,108 @@
+/*
+SQLyog Community Edition- MySQL GUI v8.03 
+MySQL - 5.6.12-log : Database - product_review
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`product_review` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `product_review`;
+
+/*Table structure for table `complaint` */
+
+DROP TABLE IF EXISTS `complaint`;
+
+CREATE TABLE `complaint` (
+  `complaint_id` int(11) NOT NULL AUTO_INCREMENT,
+  `complaint` varchar(30) DEFAULT NULL,
+  `complaint_date` date DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `reply` varchar(30) DEFAULT NULL,
+  `reply_date` date DEFAULT NULL,
+  PRIMARY KEY (`complaint_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `complaint` */
+
+insert  into `complaint`(`complaint_id`,`complaint`,`complaint_date`,`user_id`,`reply`,`reply_date`) values (1,'Need more product','2023-07-10',2,'get soon','2023-07-10'),(2,'No quality','2023-07-10',3,NULL,NULL);
+
+/*Table structure for table `login` */
+
+DROP TABLE IF EXISTS `login`;
+
+CREATE TABLE `login` (
+  `login_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) DEFAULT NULL,
+  `password` varchar(30) DEFAULT NULL,
+  `type` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`login_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `login` */
+
+insert  into `login`(`login_id`,`username`,`password`,`type`) values (1,'admin','admin','admin'),(2,'princy@gmail.com','9895','user'),(3,'akku@gmail.com','2255','user');
+
+/*Table structure for table `product` */
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(30) DEFAULT NULL,
+  `product_price` varchar(30) DEFAULT NULL,
+  `description` varchar(30) DEFAULT NULL,
+  `photo` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `product` */
+
+insert  into `product`(`product_id`,`product_name`,`product_price`,`description`,`photo`) values (1,'Biscuit','100','Cream Biscuit','/static/image/20230710_101042.jpg'),(2,'diary milk','85','chocolate candy','/static/image/20230710_150400.jpg');
+
+/*Table structure for table `review` */
+
+DROP TABLE IF EXISTS `review`;
+
+CREATE TABLE `review` (
+  `review_id` int(11) NOT NULL AUTO_INCREMENT,
+  `review` varchar(30) DEFAULT NULL,
+  `review_date` date DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`review_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `review` */
+
+insert  into `review`(`review_id`,`review`,`review_date`,`user_id`,`product_id`) values (1,'good product','2023-07-10',2,1),(2,'Need More','2023-07-10',3,2);
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) DEFAULT NULL,
+  `gender` varchar(30) DEFAULT NULL,
+  `house_name` varchar(30) DEFAULT NULL,
+  `place` varchar(30) DEFAULT NULL,
+  `post` varchar(30) DEFAULT NULL,
+  `pin` int(11) DEFAULT NULL,
+  `email` varchar(30) DEFAULT NULL,
+  `phone_no` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`user_id`,`username`,`gender`,`house_name`,`place`,`post`,`pin`,`email`,`phone_no`) values (2,'princy','Female','sreenilayam','kodiyeri','Moozhikkara',970103,'princy@gmail.com',9895747411),(3,'akshaya','Female','kripa','Kannur','kannur',670115,'akku@gmail.com',8748356210);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
